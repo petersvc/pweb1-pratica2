84dc7484e4c81523ddb9d0c5c6c8384f33cdaa74
Peter Costa <petersvcosta@gmail.com>
2023-09-03 21:50:30 -0300

    get and set dataAniversario

--- banco-js-1-contas/js/modelo/poupanca.js
+++ banco-js-1-contas/js/modelo/poupanca.js
@@ -1,16 +1,23 @@
 class Poupanca extends Conta {
     constructor(numero, saldo, dataAniversario) {
         super(numero, saldo);
-        this.dataAniversario = dataAniversario
+        this._dataAniversario = dataAniversario
     }
 
     creditar(valor) {
         const hoje = "02/05"
         let juros = 1
-        if(this.dataAniversario === hoje) {
+        if(this._dataAniversario === hoje) {
             juros = 1.1
         }
         super.creditar(valor * juros);
+    }
+
+    get dataAniversario() {
+        return this._dataAniversario
+    }
+    set dataAniversario(novaDataAniversario) {
+        this._dataAniversario = novaDataAniversario
     }
 
 }
